1d9f12133cadcb65d2bf9c78251fd3839d2cd383
Harm <mail@harmtemolder.nl>
2020-06-08 19:18:25 -0400

    Finished EPUB

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # teachastronomy-to-epub
- Download all content from the teachastronomy.com textbook to an epub file so I can read it offline
+ Download all content from the teachastronomy.com textbook to an EPUB file so I can read it offline
 
 ## It uses the following libraries, so make sure these are installed in your `env`
 * `pandas`
@@ -7,10 +7,16 @@
 * `requests`
 * `ebooklib`
 * `yattag`
+* `Pillow`
+* `cairosvg`
 
 ## Follow these steps:
 1. Use `get_index.py` to create a dataframe of dataframes (sections per chapter)
 1. Use `get_contents.py` to add the actual contents of each section to that dataframe
-1. ~~Use `create_html.py` to convert those contents to a single HTML file~~
-1. ~~Use `get_images.py` to download all referenced images and create a new, offline HTML file~~
-1. Use `create_epub.py` to convert those contents to an EPUB file and download all referenced images
+1. Use `create_epub.py` to convert those contents to an EPUB file and download, resize and convert all referenced images
+
+## Cover
+I Photoshopped a cover from the background and logo of the website into `cover.jpg`. Feel free to do the same
+
+## Copyright
+I don't store any copyrighted material here on GitHub, only the scripts I wrote to collect everything into an EPUB for personal use. All copyrights of all materials are with their respective authors
--- create_epub.py
+++ create_epub.py
@@ -4,9 +4,65 @@ import time
 import urllib.request
 
 from bs4 import BeautifulSoup
+from cairosvg import svg2png
 from ebooklib import epub
 import pandas as pd
+from PIL import Image
 from yattag import Doc, indent
+
+def handle_img(source_url):
+    # This function checks if the requested image has been downloaded and does
+    # so if it hasn't. It then resizes the image to 758px wide if it is wider. It
+    # then saves the file as a JPEG to `images_small` and returns the file path
+
+    file_name, file_ext = os.path.splitext(source_url.split('/')[-1])
+
+    file_path_original = 'images_original/{}{}'.format(file_name, file_ext)
+    file_path_small = 'images_small/{}.jpg'.format(file_name)
+
+    if not os.path.isfile(file_path_small):
+        # Download the file if it hasn't been already
+        if not os.path.isfile(file_path_original):
+            _, headers = urllib.request.urlretrieve(source_url, file_path_original)
+            # sleep for 0.5 seconds to not overload the servers with requests
+            time.sleep(0.5)
+
+        if file_ext == '.svg':
+            png_path = file_path_original.replace('.svg', '.png')
+
+            # Rasterize an SVG file to a PNG that PIL can then handle
+            svg2png(
+                url=file_path_original,
+                write_to=png_path,
+                unsafe=True
+            )
+
+            file_path_original = png_path
+
+        with Image.open(file_path_original, mode='r') as img:
+            # Resize to fit within my Kobo Aura's resolution (758×1014)
+            max_height, max_width = 1014, 758
+            img_height, img_width = img.size
+
+            if img_height > max_height or img_width > max_width:
+                height_ratio = max_height / img_height
+                width_ratio = max_width / img_width
+                ratio = min(height_ratio, width_ratio)
+                img = img.resize(
+                    size=(
+                        int(round(img_height * ratio)),
+                        int(round(img_width * ratio))),
+                    resample=Image.LANCZOS)
+                # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-filters
+
+            # Set to RGB if needed
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+
+            # This will convert the file to JPEG based if needed
+            img.save(file_path_small)
+
+    return file_path_small
 
 # The pickle file the contents should be read from
 contents = pd.read_pickle('./contents.pkl')
@@ -18,122 +74,112 @@ book = epub.EpubBook()
 book.set_identifier('teachastronomy.com')
 book.set_title('Teach Astronomy')
 book.set_language('en')
-
 book.add_author('Chris Impey')
 book.spine = ['nav'] # Chapters will be appended later
+book.set_cover('cover.jpg', open('cover.jpg', 'rb').read())
 
 # Loop through all chapters
 for chapter_index, chapter in contents.iterrows():
-  chapter_title = '{}. {}'.format(
-      chapter_index + 1,
-      chapter['humanchapter'])
-
-  # Set up en EPUB chapter...
-  epub_chapter = epub.EpubHtml(
-    title=chapter_title,
-    file_name='chap_{0:02d}.xhtml'.format(chapter_index + 1))
-
-  # ... and compile the HTML contents for it
-  doc, tag, text = Doc().tagtext()
-
-  with tag('h1'):
-    text(chapter_title)
-
-  for section_index, section in chapter['sections'].iterrows():
-    section_title = '{}.{} {}'.format(
+    chapter_title = '{}. {}'.format(
         chapter_index + 1,
-        section_index + 1,
-        section['section'])
+        chapter['humanchapter'])
 
-    # Start every section with an H2 section title
-    with tag('h2'):
-      text(section_title)
+    print('# {}'.format(chapter_title))
 
-    # Parse every paragraph of the section one-by-one
-    section_soup = BeautifulSoup(section['contents'], 'html.parser')
-    section_paragraphs = section_soup.find(id='book-page-contents').contents
+    # Set up en EPUB chapter...
+    epub_chapter = epub.EpubHtml(
+        title=chapter_title,
+        file_name='chap_{0:02d}.xhtml'.format(chapter_index + 1))
 
-    for paragraph in section_paragraphs:
-      if (hasattr(paragraph, 'attrs') and
-          'class' in paragraph.attrs and
-          ('book-paragraph-wrapper' in paragraph.attrs['class'] or
-           'astropedia-image-container' in paragraph.attrs['class'])):
+    # ... and compile the HTML contents for it
+    doc, tag, text = Doc().tagtext()
 
-        # These special paragraphs usually contain an image, ...
-        img = paragraph.find('img')  # Note that I only keep the first
+    with tag('h1'):
+        text(chapter_title)
 
-        # ... sometimes with a div with an alt attribute, ...
-        div_caption = paragraph.find('div', 'astropedia-image-caption')
+    for section_index, section in chapter['sections'].iterrows():
+        section_title = '{}.{} {}'.format(
+            chapter_index + 1,
+            section_index + 1,
+            section['section'])
 
-        if img:
-          with tag('figure'):
-            img_src = 'https:{}'.format(img['src'].replace('/hrthumbs', ''))
-            img_name = img_src.split('/')[-1]
-            img_path = 'images/{}'.format(img_name)
-            img_ext = img_name.split('.')[-1].lower()
+        print('## {}'.format(section_title))
 
-            # Download the image if it hasn't been already
-            if not os.path.isfile(img_path):
-              _, headers = urllib.request.urlretrieve(img_src, img_path)
-              print('{}: {}'.format(img_name, headers.get('Connection')))
+        # Start every section with an H2 section title
+        with tag('h2'):
+            text(section_title)
 
-              # sleep for 0.5 seconds to not overload the servers with requests
-              time.sleep(0.5)
+        # Parse every paragraph of the section one-by-one
+        section_soup = BeautifulSoup(section['contents'], 'html.parser')
+        section_paragraphs = section_soup.find(id='book-page-contents').contents
 
-            # Add the image to the EPUB
-            epub_img = epub.EpubImage()
-            epub_img.uid = img_name.split('.')[0]
-            epub_img.file_name = 'images/{}'.format(img_name)
+        for paragraph in section_paragraphs:
+            if (hasattr(paragraph, 'attrs') and
+                    'class' in paragraph.attrs and
+                    ('book-paragraph-wrapper' in paragraph.attrs['class'] or
+                     'astropedia-image-container' in paragraph.attrs['class'])):
 
-            if img_ext == 'jpg' or img_ext == 'jpeg':
-              epub_img.media_type = 'image/jpeg'
-            elif img_ext == 'png':
-              epub_img.media_type = 'image/png'
-            elif img_ext == 'gif':
-              epub_img.media_type = 'image/gif'
-            elif img_ext == 'svg':
-              epub_img.media_type = 'image/svg+xml'
-            else:
-              raise IOError('You are trying to add a non-standard image')
+                # These special paragraphs usually contain an image, ...
+                img = paragraph.find('img')  # Note that I only keep the first
 
-            with open(img_path, 'rb') as img_bin:
-              epub_img.content = img_bin.read()
+                # ... sometimes with a div with an alt attribute, ...
+                div_caption = paragraph.find('div', 'astropedia-image-caption')
 
-            book.add_item(epub_img)
+                if img:
+                    with tag('figure'):
+                        img_src = 'https:{}'.format(img['src'].replace('/hrthumbs', ''))
+                        img_path = handle_img(img_src)
+                        img_name, img_ext = os.path.splitext(img_path.split('/')[-1])
 
-            if div_caption:
-              img_caption = div_caption.text.strip()
-              doc.stag('img', src=img_path, alt=img_caption)
-              with tag('figcaption'):
-                text(img_caption)
-            else:
-              doc.stag('img', src=img_path)
+                        # Add the image to the EPUB
+                        epub_img = epub.EpubImage()
+                        epub_img.uid = img_name
+                        epub_img_path = 'images/{}{}'.format(img_name, img_ext)
+                        epub_img.file_name = epub_img_path
 
-        # ... and usually followed by a p of text
-        p = paragraph.find('p')
-        if p:
-          paragraph = p
-        else:
-          continue
+                        if img_ext == '.jpg':
+                            epub_img.media_type = 'image/jpeg'
+                        else:
+                            raise ValueError('You\'re adding something that isn\'t a JPEG')
 
-      if hasattr(paragraph, 'text'):
-        if re.search('^\s*$', str(paragraph.text)):
-          # Skip empty paragraphs
-          continue
+                        with open(img_path, 'rb') as img_bin:
+                            epub_img.content = img_bin.read()
 
-        paragraph_text = paragraph.text
-        paragraph_text = ' '.join(paragraph_text.split())
-        paragraph_text = paragraph_text.replace('\n', '')
-        paragraph_text = paragraph_text.replace(' ,', ',')
-        paragraph_text = paragraph_text.replace(' .', '.')
+                        book.add_item(epub_img)
 
-        with tag('p'):
-          text(paragraph_text)
+                        if div_caption:
+                            img_caption = div_caption.text.strip()
+                            doc.stag('img', src=epub_img_path, alt=img_caption)
+                            with tag('figcaption'):
+                                text(img_caption)
+                        else:
+                            doc.stag('img', src=epub_img_path)
 
-  # Add the HTML to the chapter and the chapter to the book
-  epub_chapter.content = indent(doc.getvalue())
-  book.add_item(epub_chapter)
-  book.spine.append(epub_chapter)
+                # ... and usually followed by a p of text
+                p = paragraph.find('p')
+                if p:
+                    paragraph = p
+                else:
+                    continue
+
+            if hasattr(paragraph, 'text'):
+                if re.search('^\s*$', str(paragraph.text)):
+                    # Skip empty paragraphs
+                    continue
+
+                paragraph_text = paragraph.text
+                paragraph_text = ' '.join(paragraph_text.split())
+                paragraph_text = paragraph_text.replace('\n', '')
+                paragraph_text = paragraph_text.replace(' ,', ',')
+                paragraph_text = paragraph_text.replace(' .', '.')
+
+                with tag('p'):
+                    text(paragraph_text)
+
+    # Add the HTML to the chapter and the chapter to the book
+    epub_chapter.content = indent(doc.getvalue())
+    book.add_item(epub_chapter)
+    book.spine.append(epub_chapter)
 
 # add default NCX and Nav file
 book.add_item(epub.EpubNcx())
